d3950981c5aa53ab088f0393379b27c92a94e93b
Robert von Massow <robert.von.massow@superluminar.io>
2022-10-12 17:42:11 +0200

    add tests (which fail ;))

--- step/tests/test_index.py
+++ step/tests/test_index.py
@@ -2,20 +2,23 @@ import os
 
 import boto3
 import moto
+from unittest.mock import ANY
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# setup environ variable before import
+os.environ['URL'] = 'http://localhost:42'
+
 
 @moto.mock_s3()
-def test_get_object_decodes_avro():
+def test_get_object_decodes_avro(mocker):
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket='test-bucket')
     with open(os.path.join(dir_path, "data", "simple.avro"), 'rb') as f:
         s3.put_object(Bucket='test-bucket', Key='simple.avro', Body=f)
 
-    # setup environ variable before import
-    os.environ['URL'] = 'http://localhost:42'
+    mocker.patch('step.index.Elasticsearch')
     from step.index import get_object
 
     it = get_object(bucket='test-bucket', key='simple.avro')
@@ -26,10 +29,10 @@ def test_get_object_decodes_avro():
 
 
 def test_ingest_into_es(mocker):
-    es_mock = mocker.patch('step.index.es')
     es_helpers_mock = mocker.patch('step.index.helpers')
-    from step.index import ingest_into_es
+    mocker.patch('step.index.Elasticsearch')
+    from step.index import ingest_into_es, es
 
-    ingest_into_es('test-index', [{'content': 'test-message'}])
+    ingest_into_es('test-index', [{'content': 'test-message'}].__iter__())
 
-    es_helpers_mock.bulk.assert_called_with(es_mock, {})
+    es_helpers_mock.bulk.assert_called_with(es, ANY)
